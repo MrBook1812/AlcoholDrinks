bd5cf2b3fcb85045c86016f421d7746a8f7ef1f1
Kirill Kniga <kiruha_95@icloud.com>
2023-03-27 15:10:27 +0300

    Initial Commit

---
+++ AlcoholDrinks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlcoholDrinks
+//
+//  Created by Kirill Kniga on 27.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
